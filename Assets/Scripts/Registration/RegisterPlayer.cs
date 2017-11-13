af27f32e617b4902ab49f77c667c9724b0c2edd5
Irtaza Mumtaz <irtazamumtaz10@gmail.com>
2017-11-13 10:51:57 +0500

    Add whole project

---
+++ Assets/Scripts/Registration/RegisterPlayer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using GameSparks.Api.Requests;
+
+public class RegisterPlayer : MonoBehaviour
+{
+
+	public InputField displayNameRegister, userNameRegister, passwordRegister;
+	public InputField userNameAuthorise, passwordAuthorise;
+	public Text displayNameResponce;
+	private bool authoriseFlag = false;
+
+	public void RegisterPlayerButton ()
+	{
+		new RegistrationRequest ()
+			.SetDisplayName (displayNameRegister.text)
+			.SetPassword (passwordRegister.text)
+			.SetUserName (userNameRegister.text)
+			.Send ((response) => {
+			if (!response.HasErrors) 
+			{
+				Debug.Log ("Player Registered");
+			} 
+			else 
+			{
+				Debug.Log ("Error Registering Player");
+			}
+		}
+		);
+	}
+
+	public void AuthorizedPlayerButton ()
+	{
+		new AuthenticationRequest ().SetUserName (userNameAuthorise.text).SetPassword (passwordAuthorise.text).Send ((response) => {
+			if (!response.HasErrors) 
+			{
+				Debug.Log ("Player Authenticated...\n");
+				displayNameResponce.text = response.DisplayName;
+
+				authoriseFlag = true;
+			} 
+			else 
+			{
+				Debug.Log ("Error Authenticating Player..." + response.Errors.JSON.ToString ());
+			}
+		});
+	}
+
+	public void GameSceneButton()
+	{
+        if (authoriseFlag) {
+            Application.LoadLevel ("AppPurchase");
+		}
+		else 
+		{
+			Debug.Log ("Error Authentication");
+		}
+	}
+
+}
